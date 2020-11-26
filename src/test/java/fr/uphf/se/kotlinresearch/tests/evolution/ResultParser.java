891b427ce80519d035a733709e0e504e93ede2b5
martinezmatias <matias.sebastian.martinez@gmail.com>
2020-11-26 17:05:55 +0100

    parsing results

--- src/test/java/fr/uphf/se/kotlinresearch/tests/evolution/ResultParser.java
+++ src/test/java/fr/uphf/se/kotlinresearch/tests/evolution/ResultParser.java
@@ -16,7 +16,18 @@ public class ResultParser {
 
 	public void parse(String folderWithResults) throws IOException {
 		File dir = new File(folderWithResults);
-		List<String> branches = new ArrayList<>();
+
+		List<String> branchesRename = new ArrayList<>();
+		List<String> masterRename = new ArrayList<>();
+
+		List<String> branchesAddKotlin = new ArrayList<>();
+		List<String> masterAddKotkin = new ArrayList<>();
+
+		List<String> branchesUpdate = new ArrayList<>();
+		List<String> masterUpdate = new ArrayList<>();
+
+		List<String> noTraditionalMigration = new ArrayList<>();
+
 		for (File aProjectFile : dir.listFiles()) {
 
 			if (!aProjectFile.getName().endsWith(".json")) {
@@ -29,28 +40,72 @@ public class ResultParser {
 			JsonElement jelement = new JsonParser().parse(reader);
 			JsonObject jobject = jelement.getAsJsonObject();
 
-			int nrMigMaster = jobject.get("nr_master_migration_rename").getAsInt();
+			int nrMigMasterRename = jobject.get("nr_master_migration_rename").getAsInt();
 
-			int nrBranches = jobject.get("nr_branches_migration_rename").getAsInt();
+			int nrBranchesRename = jobject.get("nr_branches_migration_rename").getAsInt();
 
-			System.out.println("-->" + aProjectFile.getName() + " nr mig in master " + nrMigMaster
-					+ " nr mig in branches " + nrBranches);
+			int upsmaster = jobject.get("nr_master_migration_update_java_koltin").getAsInt();
+			int upsbranches = jobject.get("nr_branches_migration_update_java_koltin").getAsInt();
 
-			if (nrBranches > 0) {
-				branches.add(aProjectFile.getName());
+			int addmaster = jobject.get("nr_master_migration_add_kotlin").getAsInt();
+
+			int addbranches = jobject.get("nr_branches_migration_add_kotlin").getAsInt();
+
+			System.out.println("-->" + aProjectFile.getName() + " nr mig in master " + nrMigMasterRename
+					+ " nr mig in branches " + nrBranchesRename + " up master " + upsmaster + " ups branches"
+					+ upsbranches + " add master " + addmaster + " add branch " + addbranches);
+
+			if (nrBranchesRename > 0) {
+				branchesRename.add(aProjectFile.getName());
+			}
+
+			if (nrMigMasterRename > 0) {
+				masterRename.add(aProjectFile.getName());
+			}
+
+			if (upsmaster > 0) {
+				masterUpdate.add(aProjectFile.getName());
+			}
+
+			if (upsbranches > 0) {
+				branchesUpdate.add(aProjectFile.getName());
+			}
+
+			if (addmaster > 0) {
+				masterAddKotkin.add(aProjectFile.getName());
+			}
+
+			if (addbranches > 0) {
+				branchesAddKotlin.add(aProjectFile.getName());
+			}
+
+			if (nrBranchesRename == 0 && nrMigMasterRename == 0
+					&& (upsmaster + upsbranches + addmaster + addbranches) > 0) {
+				noTraditionalMigration.add(aProjectFile.getName());
 			}
 
 		}
-		System.out.println("Projects with mig in branches " + branches.size() + " " + branches);
+		//
+		System.out.println("---");
+		System.out.println("Projects with mig in master " + masterRename.size() + " " + masterRename);
+		System.out.println("Projects with mig in branches " + branchesRename.size() + " " + branchesRename);
+		System.out.println("---");
+		System.out.println("Projects with ups in master " + masterUpdate.size() + " " + masterUpdate);
+		System.out.println("Projects with ups in branches " + branchesUpdate.size() + " " + branchesUpdate);
+		System.out.println("---");
+		System.out.println("Projects with add in master " + masterAddKotkin.size() + " " + masterAddKotkin);
+		System.out.println("Projects with add in branches " + branchesAddKotlin.size() + " " + branchesAddKotlin);
 
+		System.out.println("---");
+		System.out.println(
+				"project with not traditional " + noTraditionalMigration.size() + " " + noTraditionalMigration);
 	}
 
 	public static void main(String[] arg) throws IOException {
 
 		ResultParser parser = new ResultParser();
-		parser.parse("/Users/matias/develop/kotlinresearch/kotlinmigrationdiff-research/migAResults/"
-				// + "migrations22112020/"
-				+ "migrations23112020/");
+		String location = "/Users/matias/develop/kotlinresearch/kotlinmigrationdiff-research/migAResults/migration25112020/";
+		parser.parse(location);
 	}
 
 }
